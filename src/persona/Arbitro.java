a03938bbaba6eb9942efaa21ee9ec87ac0f688ff
yoseff <yoseffcajamarca@gmail.com>
2021-05-31 18:32:23 -0300

    Inicio de proyecto

---
+++ src/persona/Arbitro.java
@@ -0,0 +1,5 @@
+package persona;
+
+public class Arbitro {
+
+}
---
+++ src/persona/Persona.java
@@ -0,0 +1,70 @@
+package persona;
+
+public class Persona {
+	protected String nombre;
+	protected String apellido;
+	protected String documento;
+	protected boolean caminar;
+	protected boolean correr;
+	protected boolean frenar;
+	
+	public Persona(String nombre, String apellido, String documento, boolean caminar, boolean correr, boolean frenar) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.documento = documento;
+		this.caminar = caminar;
+		this.correr = correr;
+		this.frenar = frenar;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public boolean isCaminar() {
+		return caminar;
+	}
+
+	public void setCaminar(boolean caminar) {
+		this.caminar = caminar;
+	}
+
+	public boolean isCorrer() {
+		return correr;
+	}
+
+	public void setCorrer(boolean correr) {
+		this.correr = correr;
+	}
+
+	public boolean isFrenar() {
+		return frenar;
+	}
+
+	public void setFrenar(boolean frenar) {
+		this.frenar = frenar;
+	}
+	
+
+}
