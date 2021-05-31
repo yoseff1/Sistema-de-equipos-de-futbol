e11a627155e5d48c3c574346379f7d8993983d0d
yoseff <yoseffcajamarca@gmail.com>
2021-05-31 19:02:57 -0300

    Agregado de ctecnico y dtecnico

--- src/persona/Ctecnico.java
+++ src/persona/Ctecnico.java
@@ -1,6 +1,61 @@
 package persona;
 
-public class Ctecnico {
+public class Ctecnico extends Persona{
+	private String club;
+	private String posicion;
+	private String traerAgua;
+	private String traerPelota;
+	private boolean masajear;
+	
+	public Ctecnico(String nombre, String apellido, String documento, boolean caminar, boolean correr, boolean frenar,
+			String club, String posicion, String traerAgua, String traerPelota, boolean masajear) {
+		super(nombre, apellido, documento, caminar, correr, frenar);
+		this.club = club;
+		this.posicion = posicion;
+		this.traerAgua = traerAgua;
+		this.traerPelota = traerPelota;
+		this.masajear = masajear;
+	}
+
+	public String getClub() {
+		return club;
+	}
+
+	public void setClub(String club) {
+		this.club = club;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	public String getTraerAgua() {
+		return traerAgua;
+	}
+
+	public void setTraerAgua(String traerAgua) {
+		this.traerAgua = traerAgua;
+	}
+
+	public String getTraerPelota() {
+		return traerPelota;
+	}
+
+	public void setTraerPelota(String traerPelota) {
+		this.traerPelota = traerPelota;
+	}
+
+	public boolean isMasajear() {
+		return masajear;
+	}
+
+	public void setMasajear(boolean masajear) {
+		this.masajear = masajear;
+	}
 	
 	
 
--- src/persona/Dtecnico.java
+++ src/persona/Dtecnico.java
@@ -1,5 +1,44 @@
 package persona;
 
-public class Dtecnico {
+public class Dtecnico extends Persona{
+	private String club;
+	private String ordenes;
+	private String cambios;
+	private boolean insultarArbitro;
+	
+	public Dtecnico(String nombre, String apellido, String documento, boolean caminar, boolean correr, boolean frenar,
+			String club, String ordenes, String cambios, boolean insultarArbitro) {
+		super(nombre, apellido, documento, caminar, correr, frenar);
+		this.club = club;
+		this.ordenes = ordenes;
+		this.cambios = cambios;
+		this.insultarArbitro = insultarArbitro;
+	}
+	public String getClub() {
+		return club;
+	}
+	public void setClub(String club) {
+		this.club = club;
+	}
+	public String getOrdenes() {
+		return ordenes;
+	}
+	public void setOrdenes(String ordenes) {
+		this.ordenes = ordenes;
+	}
+	public String getCambios() {
+		return cambios;
+	}
+	public void setCambios(String cambios) {
+		this.cambios = cambios;
+	}
+	public boolean isInsultarArbitro() {
+		return insultarArbitro;
+	}
+	public void setInsultarArbitro(boolean insultarArbitro) {
+		this.insultarArbitro = insultarArbitro;
+	}
+	
+	
 
 }
