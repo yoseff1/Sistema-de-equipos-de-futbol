1ea10c0ff4622fabd352d215163b591d8273e0bf
yoseff <yoseffcajamarca@gmail.com>
2021-05-31 18:50:59 -0300

    Agregado de jugador y referee

--- src/persona/Arbitro.java
+++ src/persona/Arbitro.java
@@ -1,5 +1,93 @@
 package persona;
 
-public class Arbitro {
+public class Arbitro extends Persona{;
+		private boolean foul;
+		private boolean penal;
+		private boolean offside;
+		private boolean tarjetaAmarilla;
+		private boolean tarjetaRoja;
+		private boolean var;
+		private boolean iniciarPartido;
+		private boolean terminarPartido;
+		
+		public Arbitro(String nombre, String apellido, String documento, boolean caminar, boolean correr,
+				boolean frenar, boolean foul, boolean penal, boolean offside, boolean tarjetaAmarilla,
+				boolean tarjetaRoja, boolean var, boolean iniciarPartido, boolean terminarPartido) {
+			super(nombre, apellido, documento, caminar, correr, frenar);
+			this.foul = foul;
+			this.penal = penal;
+			this.offside = offside;
+			this.tarjetaAmarilla = tarjetaAmarilla;
+			this.tarjetaRoja = tarjetaRoja;
+			this.var = var;
+			this.iniciarPartido = iniciarPartido;
+			this.terminarPartido = terminarPartido;
+		}
+
+		public boolean isFoul() {
+			return foul;
+		}
+
+		public void setFoul(boolean foul) {
+			this.foul = foul;
+		}
+
+		public boolean isPenal() {
+			return penal;
+		}
+
+		public void setPenal(boolean penal) {
+			this.penal = penal;
+		}
+
+		public boolean isOffside() {
+			return offside;
+		}
+
+		public void setOffside(boolean offside) {
+			this.offside = offside;
+		}
+
+		public boolean isTarjetaAmarilla() {
+			return tarjetaAmarilla;
+		}
+
+		public void setTarjetaAmarilla(boolean tarjetaAmarilla) {
+			this.tarjetaAmarilla = tarjetaAmarilla;
+		}
+
+		public boolean isTarjetaRoja() {
+			return tarjetaRoja;
+		}
+
+		public void setTarjetaRoja(boolean tarjetaRoja) {
+			this.tarjetaRoja = tarjetaRoja;
+		}
+
+		public boolean isVar() {
+			return var;
+		}
+
+		public void setVar(boolean var) {
+			this.var = var;
+		}
+
+		public boolean isIniciarPartido() {
+			return iniciarPartido;
+		}
+
+		public void setIniciarPartido(boolean iniciarPartido) {
+			this.iniciarPartido = iniciarPartido;
+		}
+
+		public boolean isTerminarPartido() {
+			return terminarPartido;
+		}
+
+		public void setTerminarPartido(boolean terminarPartido) {
+			this.terminarPartido = terminarPartido;
+		}
+		
+		
 
 }
--- src/persona/Jugador.java
+++ src/persona/Jugador.java
@@ -1,5 +1,72 @@
 package persona;
 
-public class Jugador {
+public class Jugador extends Persona {
+	private String club;
+	private String dorsal;
+	private String posicion;
+	private boolean pasar;
+	private boolean eludir;
+	private boolean patear;
+	
+	public Jugador(String nombre, String apellido, String documento, boolean caminar, boolean correr, boolean frenar,
+			String club, String dorsal, String posicion, boolean pasar, boolean eludir, boolean patear) {
+		super(nombre, apellido, documento, caminar, correr, frenar);
+		this.club = club;
+		this.dorsal = dorsal;
+		this.posicion = posicion;
+		this.pasar = pasar;
+		this.eludir = eludir;
+		this.patear = patear;
+	}
+
+	public String getClub() {
+		return club;
+	}
+
+	public void setClub(String club) {
+		this.club = club;
+	}
+
+	public String getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(String dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	public boolean isPasar() {
+		return pasar;
+	}
+
+	public void setPasar(boolean pasar) {
+		this.pasar = pasar;
+	}
+
+	public boolean isEludir() {
+		return eludir;
+	}
+
+	public void setEludir(boolean eludir) {
+		this.eludir = eludir;
+	}
+
+	public boolean isPatear() {
+		return patear;
+	}
+
+	public void setPatear(boolean patear) {
+		this.patear = patear;
+	}
+	
+	
 
 }
